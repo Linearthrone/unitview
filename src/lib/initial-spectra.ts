e499c201abd042f3f4e221b7afc1b4baaeaf08a2
Kayleigh Elizabeth Wood <katelyn.rhoades34@gmail.com>
2025-07-20 00:49:27 +0000

    starting build "444080c2-4cd1-4344-a580-7a890078a6ba" FETCHSOURCE BUILD

--- src/lib/initial-spectra.ts
+++ src/lib/initial-spectra.ts
@@ -2,8 +2,5 @@
 import type { Spectra } from '@/types/nurse';
 
 export const generateInitialSpectra = (): Spectra[] => {
-  return [
-    {(706)-774-7228}
-    {(706)-774-7056}
-  ];
+  return [];
 };
